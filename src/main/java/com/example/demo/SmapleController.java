1a074938574bd558b31d69d335339d6fed2a98e8
Rhys Chang <godjdc@gmail.com>
2020-12-11 12:53:13 +0800

    code format

--- src/main/java/com/example/demo/SmapleController.java
+++ src/main/java/com/example/demo/SmapleController.java
@@ -11,22 +11,22 @@ import javax.annotation.PostConstruct;
 @RestController
 public class SmapleController {
 
-    @Autowired
-    private SampleDataDao sampleDataDao;
+  @Autowired
+  private SampleDataDao sampleDataDao;
 
-    @PostConstruct
-    public void init() {
-        sampleDataDao.save(new SampleData(null, "AAA"));
-        sampleDataDao.save(new SampleData(null, "BBB"));
-        sampleDataDao.save(new SampleData(null, "CCC"));
-        sampleDataDao.save(new SampleData(null, "DDD"));
-        sampleDataDao.save(new SampleData(null, "EEE"));
-        sampleDataDao.save(new SampleData(null, "FFF"));
-    }
+  @PostConstruct
+  public void init() {
+    sampleDataDao.save(new SampleData(null, "AAA"));
+    sampleDataDao.save(new SampleData(null, "BBB"));
+    sampleDataDao.save(new SampleData(null, "CCC"));
+    sampleDataDao.save(new SampleData(null, "DDD"));
+    sampleDataDao.save(new SampleData(null, "EEE"));
+    sampleDataDao.save(new SampleData(null, "FFF"));
+  }
 
-    @GetMapping
-    public Page<SampleData> query(Pageable pageable) {
-        return sampleDataDao.findAll(pageable);
-    }
+  @GetMapping
+  public Page<SampleData> query(Pageable pageable) {
+    return sampleDataDao.findAll(pageable);
+  }
 
 }
